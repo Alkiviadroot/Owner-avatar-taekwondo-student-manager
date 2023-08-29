447ac39bf5225ef36346d27628e028c9677b465a
Alkiviadroot <alkiviadroot@pm.me>
2023-08-29 08:04:09 +0300

    fix: genethlia carousel meres fix

--- src/routes/(core)/+page.server.ts
+++ src/routes/(core)/+page.server.ts
@@ -3,28 +3,23 @@ import { serializeNonPOJOs } from '$lib/utils.js';
 import moment from 'moment';
 
 export const load = async ({ locals }: any,) => {
-    let simeraGenethlia: any = [];
-    let anerxomenaGenethlia: any = [];
+
 
     const mathites = serializeNonPOJOs(await locals.pb.collection('mathites').getFullList({
         filter: 'energos = true',
     }));
 
     for (const mathitis of mathites) {
-        const genethlia = moment(mathitis.genethlia).format('DDMM')
-        const meres = moment(genethlia, "DDMM").fromNow().split(" ");
-        if (meres[0] == 'a')
-            mathitis.meres = 0;
-        else if (meres[0] == "in")
-            mathitis.meres = meres[1];
-        else
-            mathitis.meres = -1;
+        const genethlia = moment(mathitis.genethlia).format('MMDD')
+        const d = new Date();
+        const simera=moment(d).format('MMDD')
+        const meres=parseInt(genethlia)-parseInt(simera);
+        mathitis.meres = meres;
     }
 
     const genethlia = mathites.filter((item: any) => item.meres !== -1);
-    simeraGenethlia = genethlia.filter((item: any) => item.meres == 0);
-    anerxomenaGenethlia = genethlia.filter((item: any) => item.meres !== 0);
-
+    const simeraGenethlia = genethlia.filter((item: any) => item.meres == 0);
+    const anerxomenaGenethlia = genethlia.filter((item: any) => item.meres !== 0);
     anerxomenaGenethlia.sort((a: any, b: any) => {
         return a.meres - b.meres;
     });
